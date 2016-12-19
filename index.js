15f2a3fc5bcb15cd6be858de72d07bdaa07a7036
Ændrew Rininsland <andrew.rininsland@ft.com>
2016-12-19 17:05:47 +0000

    Finishing importer, adding new use option

--- index.js
+++ index.js
@@ -2,42 +2,137 @@
 const fs_1 = require("fs");
 const stream_1 = require("stream");
 const split2 = require("split2");
+parseSnapshot('./tmp/Prod195_1754_ew_1.dat').on('data', (data) => console.dir(data));
 function getCompanyStatus(code) {
     const ret = {
         code: code.toLowerCase(),
-        description: undefined,
+        status: '',
     };
     switch (code.toLowerCase()) {
         case 'c':
-            ret.description = 'converted/closed company';
+            ret.status = 'converted/closed company';
             return ret;
         case 'd':
-            ret.description = 'converted/closed company';
+            ret.status = 'converted/closed company';
             return ret;
         case 'l':
-            ret.description = 'converted/closed company';
+            ret.status = 'converted/closed company';
             return ret;
         case 'r':
-            ret.description = 'converted/closed company';
+            ret.status = 'converted/closed company';
             return ret;
+        case ' ':
         default:
-            ret.description = 'unknown status';
+            ret.status = 'unknown status';
             return ret;
     }
 }
 exports.getCompanyStatus = getCompanyStatus;
 exports.isPersonRecord = (line) => Number(line.charAt(8)) === 2;
 exports.isCompanyRecord = (line) => Number(line.charAt(8)) === 1;
-exports.isCorporateBody = code => code.toLowerCase() === 'y';
-function getAppointmentDateOrigin(code) { }
+exports.isCorporateBody = (code) => code.toLowerCase() === 'y';
+function getAppointmentDateOrigin(code) {
+    const result = {
+        code: Number(code),
+        source: '',
+    };
+    switch (result.code) {
+        case 1:
+            result.source = 'appointment documents (288a, AP01, AP02, AP03, AP04, RR01 NI form 296, SEAP01 or SEAP02)';
+            break;
+        case 2:
+            result.source = 'annual return (form 363)';
+            break;
+        case 3:
+            result.source = 'incorporation documents (form 10, IN01, NI form 21, SEFM01, SEFM02, SEFM03, SEFM04, SEFM05, SECV01 or SETR02)';
+            break;
+        case 4:
+            result.source = 'LLP appointment documents (LLP288a, LLAP01, LLAP02 or NI form LLP296a)';
+            break;
+        case 5:
+            result.source = 'LLP incorporation documents (LLP2 or LLIN01)';
+            break;
+        case 6:
+            result.source = 'overseas company appointment documents (BR4, OSAP01, OSAP02, OSAP03 or OSAP04)';
+            break;
+    }
+    return result;
+}
 exports.getAppointmentDateOrigin = getAppointmentDateOrigin;
-function getAppointmentType(code) { }
+function getAppointmentType(code) {
+    const result = {
+        code,
+        status: '',
+    };
+    switch (code) {
+        case '00':
+            result.status = 'Current Secretary';
+            break;
+        case '01':
+            result.status = 'Current Director';
+            break;
+        case '04':
+            result.status = 'Current non-designated LLP Member';
+            break;
+        case '05':
+            result.status = 'Current designated LLP Member';
+            break;
+        case '11':
+            result.status = 'Current Judicial Factor';
+            break;
+        case '12':
+            result.status = 'Current Receiver or Manager appointed under the Charities Act';
+            break;
+        case '13':
+            result.status = 'Current Manager appointed under the CAICE Act';
+            break;
+        case '17':
+            result.status = 'Current SE Member of Administrative Organ';
+            break;
+        case '18':
+            result.status = 'Current SE Member of Supervisory Organ';
+            break;
+        case '19':
+            result.status = 'Current SE Member of Management Organ';
+            break;
+    }
+    return result;
+}
 exports.getAppointmentType = getAppointmentType;
-function parseDate(value) { }
+function parseDate(value) {
+    const dateString = value.trim();
+    const cc = dateString.substr(0, 2);
+    const yy = dateString.substr(2, 2);
+    const mm = dateString.substr(4, 2);
+    const dd = dateString.substr(6, 2);
+    return `${cc}${yy}-${mm}${dd ? '-' + dd : ''}`;
+}
 exports.parseDate = parseDate;
-function parseDOB(partial, full) { }
+function parseDOB(partial, full) {
+    if (full.trim())
+        return { type: 'full', date: parseDate(full) };
+    else
+        return { type: 'partial', date: parseDate(partial) };
+}
 exports.parseDOB = parseDOB;
-function parseVariableData(data) { }
+function parseVariableData(data) {
+    const items = data.split('<');
+    return {
+        title: items[0] || null,
+        forenames: items[1] || null,
+        surname: items[2] || null,
+        honours: items[3] || null,
+        careOf: items[4] || null,
+        POBox: items[5] || null,
+        address: `${items[6]}${items[7] ? '\n' + items[7] : ''}` || null,
+        town: items[8] || null,
+        county: items[9] || null,
+        country: items[10] || null,
+        occupation: items[11] || null,
+        nationality: items[12] || null,
+        countryOfResidence: items[13] || null,
+    };
+}
 exports.parseVariableData = parseVariableData;
 function parseCompanyRecord(line) {
     return {
@@ -65,32 +160,60 @@ function parsePersonRecord(line) {
     };
 }
 exports.parsePersonRecord = parsePersonRecord;
-function parseSnapshot(path) {
+function parseSnapshot(path, condenseRecords = false) {
     if (!path) {
         throw new Error('No file path specified');
     }
-    let currentRecord;
-    const parseLine = new stream_1.Transform({
-        objectMode: true,
-        transform(line, encoding, callback) {
-            if (exports.isCompanyRecord(line)) {
-                if (currentRecord) {
-                    this.push(currentRecord);
+    if (!condenseRecords) {
+        const parseLine = new stream_1.Transform({
+            objectMode: true,
+            transform(_line, encoding, callback) {
+                const line = _line.toString();
+                if (exports.isCompanyRecord(line)) {
+                    callback(null, {
+                        type: 'company',
+                        source: 'snapshot',
+                        record: parseCompanyRecord(line),
+                    });
                 }
-                currentRecord = parseCompanyRecord(line);
+                else if (exports.isPersonRecord(line)) {
+                    const record = parsePersonRecord(line);
+                    record.companyNumber = line.substr(0, 8);
+                    callback(null, {
+                        type: 'person',
+                        source: 'snapshot',
+                        record,
+                    });
+                }
             }
-            else if (exports.isPersonRecord(line)) {
-                currentRecord.officers.records.push(parsePersonRecord(line));
+        });
+        return fs_1.createReadStream(path)
+            .pipe(split2())
+            .pipe(parseLine);
+    }
+    else {
+        let currentRecord;
+        const parseLine = new stream_1.Transform({
+            objectMode: true,
+            transform(_line, encoding, callback) {
+                const line = _line.toString();
+                if (exports.isCompanyRecord(line)) {
+                    if (currentRecord) {
+                        this.push(currentRecord);
+                    }
+                    currentRecord = parseCompanyRecord(line);
+                }
+                else if (exports.isPersonRecord(line)) {
+                    currentRecord.officers.records.push(parsePersonRecord(line));
+                }
+                callback();
             }
-            callback();
-        }
-    });
-    fs_1.createReadStream(path)
-        .pipe(split2())
-        .pipe(parseLine)
-        .on('data', record => console.dir(record));
+        });
+        return fs_1.createReadStream(path)
+            .pipe(split2())
+            .pipe(parseLine);
+    }
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = parseSnapshot;
 ;
-parseSnapshot('./tmp/Prod195_1754_ew_1.dat');
--- index.ts
+++ index.ts
@@ -8,42 +8,205 @@ import { createReadStream } from 'fs';
 import { Transform } from 'stream';
 import * as split2 from 'split2';
 
-export function getCompanyStatus(code) {
+
+
+// To test, uncomment:
+parseSnapshot('./tmp/Prod195_1754_ew_1.dat').on('data', (data) => console.dir(data));
+
+
+
+
+/**
+ * Get company status from code
+ * @param  {string} code Company status code. One of C, D, L, R or (space).
+ * @return {CompaniesHouseCodeWithValue}      An object containing the code (lowercase) and its description.
+ */
+export function getCompanyStatus(code: string): CompaniesHouseCodeWithValue {
   const ret = {
     code: code.toLowerCase(),
-    description: undefined,
+    status: '',
   };
 
   switch (code.toLowerCase()) {
     case 'c':
-      ret.description = 'converted/closed company';
+      ret.status = 'converted/closed company';
       return ret;
     case 'd':
-      ret.description = 'converted/closed company';
+      ret.status = 'converted/closed company';
       return ret;
     case 'l':
-      ret.description = 'converted/closed company';
+      ret.status = 'converted/closed company';
       return ret;
     case 'r':
-      ret.description = 'converted/closed company';
+      ret.status = 'converted/closed company';
       return ret;
+    case ' ':
     default:
-      ret.description = 'unknown status';
+      ret.status = 'unknown status';
       return ret;
   }
 }
 
-export const isPersonRecord = (line) => Number(line.charAt(8)) === 2; // Eighth character is record type. "1" for person.
-export const isCompanyRecord = (line) => Number(line.charAt(8)) === 1; // Eighth character is record type. "1" for company.
-export const isCorporateBody = code => code.toLowerCase() === 'y';
+/**
+ * Checks whether line indicates the following record is for a person
+ * @param  {string} line          First line of a record
+ * @return {boolean}              Whether record is a snaptshot person record
+ */
+export const isPersonRecord = (line: string) => Number(line.charAt(8)) === 2; // Eighth character is record type. "2" for person
 
-export function getAppointmentDateOrigin(code) {}
-export function getAppointmentType(code) {}
-export function parseDate(value) {}
-export function parseDOB(partial, full) {}
-export function parseVariableData(data) {}
+/**
+ * Checks whether line indicates the following record is for a company
+ * @param  {string} line          First line of a record
+ * @return {boolean}              Whether record is a snaptshot company record
+ */
+export const isCompanyRecord = (line: string) => Number(line.charAt(8)) === 1; // Eighth character is record type. "1" for company
 
-export function parseCompanyRecord(line) {
+/**
+ * Checks whether a director is a corporate body (i.e., value is 'Y')
+ * @param {string} code           Either a string containing 'Y' or empty string
+ * @return {boolean}              Whether director is a corporate body
+ */
+export const isCorporateBody = (code: string) => code.toLowerCase() === 'y';
+
+/**
+ * Parse an appointment date origin code into an object
+ * @param  {string} code  Appointment date origin code
+ * @return {CompaniesHouseCodeWithValue}      Object containing code and description
+ */
+export function getAppointmentDateOrigin(code: string): CompaniesHouseCodeWithValue {
+  const result = {
+    code: Number(code),
+    source: '',
+  };
+
+  switch (result.code) {
+    case 1:
+      result.source = 'appointment documents (288a, AP01, AP02, AP03, AP04, RR01 NI form 296, SEAP01 or SEAP02)';
+      break;
+    case 2:
+      result.source = 'annual return (form 363)';
+      break;
+    case 3:
+      result.source = 'incorporation documents (form 10, IN01, NI form 21, SEFM01, SEFM02, SEFM03, SEFM04, SEFM05, SECV01 or SETR02)';
+      break;
+    case 4:
+      result.source = 'LLP appointment documents (LLP288a, LLAP01, LLAP02 or NI form LLP296a)';
+      break;
+    case 5:
+      result.source = 'LLP incorporation documents (LLP2 or LLIN01)';
+      break;
+    case 6:
+      result.source = 'overseas company appointment documents (BR4, OSAP01, OSAP02, OSAP03 or OSAP04)';
+      break;
+  }
+
+  return result;
+}
+
+/**
+ * Parse an appointment type from code
+ * @param  {string}                      code Appointment type code
+ * @return {CompaniesHouseCodeWithValue}      Object containing appointment type code and description
+ */
+export function getAppointmentType(code: string): CompaniesHouseCodeWithValue {
+  const result = {
+    code, // Not typed to Number here to preserve leading zero
+    status: '',
+  };
+
+  switch (code) {
+    case '00':
+      result.status = 'Current Secretary';
+      break;
+    case '01':
+      result.status = 'Current Director';
+      break;
+    case '04':
+      result.status = 'Current non-designated LLP Member';
+      break;
+    case '05':
+      result.status = 'Current designated LLP Member';
+      break;
+    case '11':
+      result.status = 'Current Judicial Factor';
+      break;
+    case '12':
+      result.status = 'Current Receiver or Manager appointed under the Charities Act';
+      break;
+    case '13':
+      result.status = 'Current Manager appointed under the CAICE Act';
+      break;
+    case '17':
+      result.status = 'Current SE Member of Administrative Organ';
+      break;
+    case '18':
+      result.status = 'Current SE Member of Supervisory Organ';
+      break;
+    case '19':
+      result.status = 'Current SE Member of Management Organ';
+      break;
+  }
+
+  return result;
+}
+
+/**
+ * Parse a date value into quasi-ISO-8601 format
+ * @param  {string} value Date string
+ * @return {string}       Quasi-ISO-8601 date format string,
+ *                        either yyyy-mm-dd or yyyy-mm depending on `value`
+ */
+export function parseDate(value: string) {
+  const dateString = value.trim();
+  const cc = dateString.substr(0, 2);
+  const yy = dateString.substr(2, 2);
+  const mm = dateString.substr(4, 2);
+  const dd = dateString.substr(6, 2); // Will be empty string if partial date.
+
+  return `${cc}${yy}-${mm}${dd ? '-' + dd : ''}`;
+}
+
+/**
+ * Parse full and/or partial DOB into a single string
+ * @param  {string} partial Partial DOB string in YYYYMM format
+ * @param  {string} full    Full DOB string in YYYYMMDD format
+ * @return {string}         Date string in quasi-ISO-8601 format (YYYY-MM-DD, or YYYY-MM if partial only)
+ */
+export function parseDOB(partial: string, full: string) {
+  if (full.trim()) return {type: 'full', date: parseDate(full) };
+  else return {type: 'partial', date: parseDate(partial) };
+}
+
+/**
+ * Parse Companies House's weird variable date format
+ * @param  {string} data                     Variable data string
+ * @return {CompaniesHouseVariableData}      Parsed variable data object
+ */
+export function parseVariableData(data: string): CompaniesHouseVariableData {
+  const items = data.split('<');
+  return {
+    title: items[0] || null,
+    forenames: items[1] || null,
+    surname: items[2] || null,
+    honours: items[3] || null,
+    careOf: items[4] || null,
+    POBox: items[5] || null,
+    address: `${items[6]}${items[7] ? '\n' + items[7] : ''}` || null,
+    town: items[8] || null,
+    county: items[9] || null,
+    country: items[10] || null,
+    occupation: items[11] || null,
+    nationality: items[12] || null,
+    countryOfResidence: items[13] || null,
+  };
+}
+
+/**
+ * Create Companies House company record from snapshot data
+ * @param  {string} line Line from a Companies House snapshot dump
+ * @return {CompaniesHouseRecordCompany}      Company record
+ */
+export function parseCompanyRecord(line: string): CompaniesHouseRecordCompany {
   return {
     name: line.substr(40).replace('<', '').trim(),
     number: line.substr(0, 8).trim(),
@@ -55,7 +218,12 @@ export function parseCompanyRecord(line) {
   };
 }
 
-export function parsePersonRecord(line) {
+/**
+ * Create Companies House person record from snapshot data
+ * @param  {string}                     line Line from a Companies House snapshot dump
+ * @return {CompaniesHouseRecordPerson}      Person record
+ */
+export function parsePersonRecord(line: string): CompaniesHouseRecordPerson {
   return {
     origin: getAppointmentDateOrigin(line.substr(9, 1)),
     type: getAppointmentType(line.substr(10, 2)),
@@ -69,54 +237,115 @@ export function parsePersonRecord(line) {
   };
 }
 
-
-export default function parseSnapshot(path: string): any {
+/**
+ * Parse a snapshot into a stream of records
+ * @param  {string} path            Path to snapshot file to parse
+ * @param  {string} condenseRecords Whether to condense company records into a single entry (WIP)
+ * @return {stream}                 ✨ ...a magical stream of records! ✨
+ */
+export default function parseSnapshot(path: string, condenseRecords = false): any {
   if (!path) {
     throw new Error('No file path specified');
   }
 
-  let currentRecord;
-
-  const parseLine = new Transform({
-    objectMode: true,
-    transform(line, encoding, callback) {
-      if (isCompanyRecord(line)) {
-        if (currentRecord) { // Push the last company object if it's a new companyRow.
-          this.push(currentRecord);
+  if (!condenseRecords) {
+    const parseLine = new Transform({
+      objectMode: true,
+      transform(_line, encoding, callback) {
+        const line = _line.toString(); // Received as string|Buffer otherwise
+        if (isCompanyRecord(line)) {
+          callback(null, {
+            type: 'company',
+            source: 'snapshot',
+            record: parseCompanyRecord(line),
+          });
+        } else if (isPersonRecord(line)) {
+          const record = parsePersonRecord(line);
+          record.companyNumber = line.substr(0, 8);
+          callback(null, {
+            type: 'person',
+            source: 'snapshot',
+            record,
+          });
         }
-        currentRecord = parseCompanyRecord(line);
-      } else if (isPersonRecord(line)) {
-        currentRecord.officers.records.push(parsePersonRecord(line));
       }
-      callback();
-    }
-  });
+    });
 
-  createReadStream(path)
-    .pipe(split2())
-    .pipe(parseLine)
-    .on('data', record => console.dir(record));
+    return createReadStream(path)
+      .pipe(split2())
+      .pipe(parseLine);
+  }
 
-  // try {
-  //   const data = readFileSync(path, {encoding: 'utf8'});
-  //   const rows = data.split('\n');
-  //   console.dir(rows[0]);
-  //   const headerRow = rows.shift();
-  //   const header = {
-  //     identifier: headerRow.substr(0, 8),
-  //     runNumber: headerRow.substr(9, 4),
-  //     date: headerRow.substr(13, 8)
-  //   };
-  //   console.dir(header);
-  //   return [header];
-  // } catch (e) {
-  //   console.error(e);
-  // }
+  // This might not work as it's entirely dependent upon records being in sequential order
+  else {
+    let currentRecord;
 
-  // const companies = [];
-  // rows.forEach(row, indx => {
-  //
-  // });
+    const parseLine = new Transform({
+      objectMode: true,
+      transform(_line, encoding, callback) {
+        const line = _line.toString(); // Received as string|Buffer otherwise
+        if (isCompanyRecord(line)) {
+          if (currentRecord) { // Push the last company object if it's a new companyRow
+            this.push(currentRecord);
+          }
+          currentRecord = parseCompanyRecord(line);
+        } else if (isPersonRecord(line)) {
+          currentRecord.officers.records.push(parsePersonRecord(line));
+        }
+        callback();
+      }
+    });
+
+    return createReadStream(path)
+      .pipe(split2())
+      .pipe(parseLine);
+  }
 };
 
-parseSnapshot('./tmp/Prod195_1754_ew_1.dat');
+interface CompaniesHouseCodeWithValue {
+  code: string | number;
+  status?: string;
+  source?: string;
+}
+
+interface CompaniesHouseVariableData {
+  title: string | null;
+  forenames: string | null;
+  surname: string | null;
+  honours: string | null;
+  careOf: string | null;
+  POBox: string | null;
+  address: string | null;
+  town: string | null;
+  county: string | null;
+  country: string | null;
+  occupation: string | null;
+  nationality: string | null;
+  countryOfResidence: string | null;
+}
+
+interface CompaniesHouseRecordCompany {
+  name: string;
+  number: string;
+  status: CompaniesHouseCodeWithValue;
+  officers: {
+    count: number;
+    records: Array<CompaniesHouseRecordPerson>;
+  };
+}
+
+interface CompaniesHouseRecordPerson {
+  companyNumber?: string;
+  origin: CompaniesHouseCodeWithValue;
+  type: CompaniesHouseCodeWithValue;
+  number: string;
+  isCorporateBody: boolean;
+  appointmentDate: string;
+  resignationDate: string;
+  postcode: string;
+  dob: {
+    type: string;
+    date: string;
+  };
+  variableData: CompaniesHouseVariableData;
+}
